042dafe347987a685d863e26b57b4ca3c407f1f4
piotr2b <pierre.deboisset@telecom-paristech.fr>
2015-05-06 13:21:26 +0200

    Quickly set Activator at school

---
+++ .set-hakka.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Download and set up the shell for Activator.
+# This must be executed like `. .set-hakka.sh` or `source .set-hakka.sh`.
+# If some trouble happens, remove directory and zip archive then try again.
+#
+current=`pwd`
+version="1.3.2"
+# This directory has more free space than home folder.
+directory="/tmp"
+cd $directory
+if [ ! -f "$directory/typesafe-activator-$version" ];
+then
+  if [ ! -f "$directory/typesafe-activator-$version.zip" ];
+  then
+    echo Downloading…
+    wget --progress=bar http://downloads.typesafe.com/typesafe-activator/1.3.2/typesafe-activator-$version.zip
+  fi
+  echo Extracting…
+  unzip -q typesafe-activator-$version.zip -d typesafe-activator-$version
+fi
+PATH=$PATH:$directory/typesafe-activator-$version
+echo Set up finished.
+cd $current
